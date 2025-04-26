fe6f394b7b805dea3be103b79172c9a85a439679
John Treon <jht3250@rit.edu>
2025-04-25 21:05:42 -0400

    fixed quotes

--- LAB5/add_user.py
+++ LAB5/add_user.py
@@ -84,7 +84,7 @@ def process_csv(file_path):
                     add_user(username, group, home_dir, shell)
 
                     # Display success message
-                    print(f"Processing employee ID {employee_id:>8}.\t '\033[32m{username:>20}\033[0m' added to system.")
+                    print(f"Processing employee ID {employee_id:>8}.\t \033[32m{username:>20}\033[0m added to system.")
                     time.sleep(1)
 
                 except ValueError as e:
